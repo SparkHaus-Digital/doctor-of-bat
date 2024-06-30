3eac8de2a513e17770a1c9532be1fc7f3c5941d2
HirushaRukmal <hirusharukmal266@gmail.com>
2024-06-30 13:21:45 +0530

    changes

--- src/screens/Aboutus.jsx
+++ src/screens/Aboutus.jsx
@@ -36,19 +36,19 @@ const AboutUsPage = () => {
 			<div className="about-us-page">
 				<div className="about-us-section">
 					<div className="about-us-left">
-						<h2 className="section-title">Behind the success</h2>
+						<h2 className="section-title2">Behind the success</h2>
 						<p className="section-description">
 							Description about the company goes here.
 						</p>
 					</div>
 					<div className="about-us-right">
+						<img src={OwnerImage} alt="Owner" className="owner-image" />
 						<h2 className="section-title">Dinura Abeygunawardane</h2>
 						<p className="owner-title">Founder of Doctor of Bat</p>
-						<img src={OwnerImage} alt="Owner" className="owner-image" />
 					</div>
 				</div>
 				<div className="testimonial-section">
-					<h2 className="section-title">People that worked with us</h2>
+					<h2 className="section-title2">People that worked with us</h2>
 					{/* <TestimonialSlider /> */}
 					<div className="slider-div">
 						<center>
